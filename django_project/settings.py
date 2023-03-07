4b8122325e8e111578b34e9c8b75802d31290e9a
Elnaz <elnaz88miri@gmail.com>
2023-03-07 14:28:36 -0500

    add login and logout

--- django_project/settings.py
+++ django_project/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
+LOGIN_REDIRECT_URL = 'blog-home'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
