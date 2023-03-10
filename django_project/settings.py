e7e056056c7b8bfeceb81516c649c0d86cfc13d7
Elnaz <elnaz88miri@gmail.com>
2023-03-09 23:06:03 -0500

    Added reseting password with an email

--- django_project/settings.py
+++ django_project/settings.py
@@ -129,6 +129,14 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='elnaz88miri@gmail.com'
+# EMAIL_HOST_USER=os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD='your_password_for_email'
+# EMAIL_HOST_PASSWORD=os.getenv('EMAIL_PASSWORD')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
